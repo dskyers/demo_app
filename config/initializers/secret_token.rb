784ee3812b0837d7b10e9865b40a9b431b6c482a
dwayne <dwayne@vitrue.com>
2013-05-11 15:19:22 -0400

    initial import the depot app from the 'Agile Web Development with Rails' book (up to chapter7)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1963fad61116f0a126b23695629691d96f5d5301d89ef6027e48519a1816efd046c2fe62cbdda2e40c89950720210907f67414b400309a76259ca3a7f1522a8d'
